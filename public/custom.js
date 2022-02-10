b0f0281489e338f65e67f57308c10a9c04928d1b
Bralex Martinez <brayanlexis.p6@gmail.com>
2022-02-10 10:32:11 -0600

    fix query selector

--- public/custom.js
+++ public/custom.js
@@ -1,11 +1,11 @@
 
 (function() {
   setTimeout(() => {  
-    $('.Banner__content--title').innerHTML="Las aventuras de Liz y Brayan"
-    $('.Banner__content--description').innerHTML="Cuando dos personas se quieren con el corazón es inevitable su reencuentro. Revive la historia de estos dos intrepidos enamorados."
-    $('header.Banner').style='background-image: url("https://i.pinimg.com/564x/f5/bd/0c/f5bd0c23d97d8fa6f9c2a0395bf0a8eb.jpg"); opacity: 1; will-change: opacity, transform';
+    document.querySelector('.Banner__content--title').innerHTML="Las aventuras de Liz y Brayan"
+    document.querySelector('.Banner__content--description').innerHTML="Cuando dos personas se quieren con el corazón es inevitable su reencuentro. Revive la historia de estos dos intrepidos enamorados."
+    document.querySelector('header.Banner').style='background-image: url("https://i.pinimg.com/564x/f5/bd/0c/f5bd0c23d97d8fa6f9c2a0395bf0a8eb.jpg"); opacity: 1; will-change: opacity, transform';
 
-    let button=$('a.Banner__button')
+    let button=document.querySelector('a.Banner__button')
     button.addEventListener('click',(e)=>{
         e.preventDefault();
         e.stopPropagation();
